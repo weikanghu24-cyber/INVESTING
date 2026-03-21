23557dd688060a27ce57e0bd5f31bca7fcf0ffbc
weikanghu <weikanghu24@gmail.com>
2026-03-21 14:31:03 +0100

    implementación de los métodos auth de register logout y ver perfil también sus correspodientes endpoints

--- TRADING/urls.py
+++ TRADING/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import AssetDetailView,AssetTickerDetail,AssetsHistoryView
+from .views import AssetDetailView,AssetTickerDetail,AssetsHistoryView,RegisterView,UserProfileView, LogoutView
 
 urlpatterns = [
-# Autenticación JWT API
+    # Autenticación JWT API
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Registar usuario, cerrar sesión y ver perfil
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
+    path('auth/me/', UserProfileView.as_view(), name='user_profile'),
     
     # API Activos Financieros
     path('assets/<str:ticker>/detail/', AssetTickerDetail.as_view(), name='asset_ticker_detail'),
